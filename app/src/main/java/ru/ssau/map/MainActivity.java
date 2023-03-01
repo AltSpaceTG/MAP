067a211dcc5ffb98b1130998b051216cecb6b58d
AltSpaceTG <domhomeloped@gmail.com>
2023-03-01 18:02:01 +0300

    add some functions to the button

--- app/src/main/java/ru/ssau/map/MainActivity.java
+++ app/src/main/java/ru/ssau/map/MainActivity.java
@@ -1,7 +1,6 @@
 package ru.ssau.map;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -12,40 +11,37 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        ConstraintLayout constraintLayout = new ConstraintLayout(this);
 
-        Button button = new Button(this);
-        button.setText("Navigate to '1'");
-        button.setId(View.generateViewId());
-
-        ConstraintLayout.LayoutParams buttonLayout = new ConstraintLayout.LayoutParams
-                (ConstraintLayout.LayoutParams.WRAP_CONTENT , ConstraintLayout.LayoutParams.WRAP_CONTENT);
-        buttonLayout.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
-        button.setLayoutParams(buttonLayout);
-        constraintLayout.addView(button);
-
+        Button button = findViewById(R.id.First);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String url = "https://yandex.ru/maps/-/CCUKAHTgSC";
-                Intent intentYandex = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                intentYandex.setPackage("ru.yandex.yandexmaps");
 
-                String uriGoogle = "https://goo.gl/maps/3NQCj3NxgYujDtso6";
-                Intent intentGoogle = new Intent(Intent.ACTION_VIEW, Uri.parse(uriGoogle));
-                intentGoogle.setPackage("com.google.android.apps.maps");
+                openMap();
+                setContentView(R.layout.study1);
 
-                String title = "Select";
-                Intent chooserIntent = Intent.createChooser(intentGoogle, title);
-                Intent[] arr = new Intent[1];
-                arr[0] = intentYandex;
-                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, arr);
-                startActivity(chooserIntent);
             }
         });
+    }
 
-        setContentView(constraintLayout);
+    private void openMap() {
+        String url = "https://yandex.ru/maps/-/CCUKAHTgSC";
+        Intent intentYandex = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        intentYandex.setPackage("ru.yandex.yandexmaps");
+
+        String uriGoogle = "https://goo.gl/maps/3NQCj3NxgYujDtso6";
+        Intent intentGoogle = new Intent(Intent.ACTION_VIEW, Uri.parse(uriGoogle));
+        intentGoogle.setPackage("com.google.android.apps.maps");
+
+        String title = "Select";
+        Intent chooserIntent = Intent.createChooser(intentGoogle, title);
+        Intent[] arr = new Intent[1];
+        arr[0] = intentYandex;
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, arr);
+        startActivity(chooserIntent);
     }
 }
