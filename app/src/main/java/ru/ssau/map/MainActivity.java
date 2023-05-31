99ed8cb29edb63a39c64a90316a3da20040f1aba
AltSpaceTG <domhomeloped@gmail.com>
2023-05-31 14:39:15 +0400

    WebView

--- app/src/main/java/ru/ssau/map/MainActivity.java
+++ app/src/main/java/ru/ssau/map/MainActivity.java
@@ -1,51 +1,26 @@
 package ru.ssau.map;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
 
-        ConstraintLayout constraintLayout = new ConstraintLayout(this);
-
-        Button button = new Button(this);
-        button.setText("Navigate to '1'");
-        button.setId(View.generateViewId());
-
-        ConstraintLayout.LayoutParams buttonLayout = new ConstraintLayout.LayoutParams
-                (ConstraintLayout.LayoutParams.WRAP_CONTENT , ConstraintLayout.LayoutParams.WRAP_CONTENT);
-        buttonLayout.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
-        button.setLayoutParams(buttonLayout);
-        constraintLayout.addView(button);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String url = "https://yandex.ru/maps/-/CCUKAHTgSC";
-                Intent intentYandex = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                intentYandex.setPackage("ru.yandex.yandexmaps");
-
-                String uriGoogle = "https://goo.gl/maps/3NQCj3NxgYujDtso6";
-                Intent intentGoogle = new Intent(Intent.ACTION_VIEW, Uri.parse(uriGoogle));
-                intentGoogle.setPackage("com.google.android.apps.maps");
-
-                String title = "Select";
-                Intent chooserIntent = Intent.createChooser(intentGoogle, title);
-                Intent[] arr = new Intent[1];
-                arr[0] = intentYandex;
-                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, arr);
-                startActivity(chooserIntent);
-            }
-        });
-
-        setContentView(constraintLayout);
+        myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("https://map.sdrvsg.xyz");
     }
 }
---
+++ app/src/main/java/ru/ssau/map/MyWebViewClient.java
@@ -0,0 +1,13 @@
+package ru.ssau.map;
+
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MyWebViewClient extends WebViewClient {
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        view.loadUrl(url);
+        return true;
+    }
+}
